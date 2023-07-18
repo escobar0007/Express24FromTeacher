9c824266a0dcb68f0806dcdafd3f8a86103695b1
Hamza <hamzamaxmudov@MacBook-Pro-Hamza.local>
2023-07-18 11:51:04 +0500

    in main created User object

--- src/uz/escobar/express24/Express24APP.java
+++ src/uz/escobar/express24/Express24APP.java
@@ -1,11 +1,13 @@
 package uz.escobar.express24;
 
+import uz.escobar.express24.model.User;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Express24APP {
     public static void main(String[] args) {
-
+        User user = new User();
     }
 
     public static Scanner sc() {
